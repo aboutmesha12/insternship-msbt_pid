cbc7317f1f97704b0eacd3bd105d0dfeb67774fe
schatten <valesharo731@gmail.com>
2026-03-09 21:28:25 +0700

    apa

---
+++ nama_nrp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string nama, nrp;
+    nama = "Alfasha Firfiana Aryanti";
+    nrp = "2040251036";
+
+    cout << "Nama   : " << nama << endl;
+    cout << "NRP    : 2040251036\n";
+    return 0;
+
+}
